171635a4a0c6a0300830c53574a5e3d6f6f3ffa3
Shubham Gupta <shubhamvmgupta@gmail.com>
2026-06-16 19:16:15 +0530

    Fix task routes for Render

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,10 +1,8 @@
 const express = require("express")
 const fs = require("fs")
+const path = require("path")
 
 const router = express.Router()
-
-const fs = require("fs")
-const path = require("path")
 
 const filePath =
 path.join(
@@ -18,97 +16,145 @@ router.get("/", (req, res) => {
 
 const tasks =
 JSON.parse(
-fs.readFileSync(path)
+fs.readFileSync(
+filePath,
+"utf8"
+)
 )
 
 res.json(tasks)
 
 })
+
 router.post("/", (req, res) => {
 
 const tasks =
 JSON.parse(
-fs.readFileSync(path)
+fs.readFileSync(
+filePath,
+"utf8"
+)
 )
 
 const newTask = {
+
 id: Date.now(),
-title: req.body.title,
-description: req.body.description,
+
+title:
+req.body.title,
+
+description:
+req.body.description,
+
 completed: false
+
 }
 
 tasks.push(newTask)
 
 fs.writeFileSync(
-path,
+
+filePath,
+
 JSON.stringify(
 tasks,
 null,
 2
 )
+
 )
 
 res.json(newTask)
 
 })
+
 router.put("/:id", (req, res) => {
 
 let tasks =
 JSON.parse(
-fs.readFileSync(path)
+fs.readFileSync(
+filePath,
+"utf8"
+)
 )
 
-tasks = tasks.map(task =>
+tasks =
+tasks.map(
+task =>
 
-task.id == req.params.id
-? {
+task.id ==
+req.params.id
+
+?
+
+{
 ...task,
 ...req.body
 }
-: task
+
+:
+
+task
 
 )
 
 fs.writeFileSync(
-path,
+
+filePath,
+
 JSON.stringify(
 tasks,
 null,
 2
 )
+
 )
 
 res.json({
-message:"updated"
+message:
+"updated"
 })
 
 })
+
 router.delete("/:id", (req, res) => {
 
 let tasks =
 JSON.parse(
-fs.readFileSync(path)
+fs.readFileSync(
+filePath,
+"utf8"
+)
 )
 
 tasks =
 tasks.filter(
+
 task =>
-task.id != req.params.id
+
+task.id !=
+req.params.id
+
 )
 
 fs.writeFileSync(
-path,
+
+filePath,
+
 JSON.stringify(
 tasks,
 null,
 2
 )
+
 )
 
 res.json({
-message:"deleted"
+message:
+"deleted"
 })
 
 })
-module.exports = router
+
+module.exports =
+router
