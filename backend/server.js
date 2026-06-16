a7fbdcb0adf0f5346bc0fed8f378ca3c0b74accf
Shubham Gupta <shubhamvmgupta@gmail.com>
2026-06-16 19:03:58 +0530

    Add Render dynamic port

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,17 @@ const taskRoutes = require("./routes/taskRoutes")
 
 app.use("/tasks", taskRoutes)
 
-app.listen(5000, () => {
-  console.log("Server running on 5000")
+const PORT =
+process.env.PORT
+||
+5000
+
+app.listen(
+PORT,
+() => {
+
+console.log(
+`Server running on ${PORT}`
+)
+
 })
